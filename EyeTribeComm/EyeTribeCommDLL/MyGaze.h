3579ceea93901e0c0378e0c8516db06a9cdbaf49
Trevor Stavropoulos <Trevor.Stavropoulos@yale.edu>
2016-02-23 09:57:26 -0500

    Adding GazeAPI licenses, headers, and binaries.  Adding Boost License.

--- EyeTribeComm/EyeTribeCommDLL/MyGaze.h
+++ EyeTribeComm/EyeTribeCommDLL/MyGaze.h
@@ -6,27 +6,40 @@
 
 namespace EyeTribeComm
 {
+	//This class implements the IGazeListener in order to receive gaze updates
 	class MyGaze : public gtl::IGazeListener
 	{
 	public:
+		//Initializes local variables
 		MyGaze();
+		//Disconnects if still connected to the GazeAPI
 		~MyGaze();
 
+		//Connects to the GazeAPI and adds self as a listener of gaze data
 		void init();
 
+		//Queries whether new gaze data is available
 		bool getNewGazeData();
+
+		//Returns the latest gaze data, along with a value describing the quality
 		void getLatestGazeData(float &a_rfX, float &a_rfY, int &a_riQuality);
 
+		//Removes self as a listener of gaze data and disconencts from the GazeAPI
 		void disconnect();
 	private:
-		//IGazeListener
+		//IGazeListener update function
 		void on_gaze_data(gtl::GazeData const &gaze_data);
 
 		gtl::GazeApi m_api;
 
+		//Holds the last gaze data
 		gtl::Point2D m_p2dLastGazePoint;
+
+		//Holds whether the object is connected to the GazeAPI
 		bool m_bInitialized;
+		//Stores whether new gaze data is avialable
 		bool m_bNewGazeData;
+		//Stores the quality of the last gaze data
 		int m_iQuality;
 	};
 }
